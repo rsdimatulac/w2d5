1015d83de801e870315556a91b773cd2e8fb54e9
Renerose Dimatulac <rsdimatulac@gmail.com>
2021-01-22 16:35:08 -0600

    03 rest sum

--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -10,7 +10,22 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// your code here!
+const arrowRestSum = (...nums) => {
+
+  let sum = 0; // counter
+
+  nums.forEach(function(ele, i , array){
+    let eachNum = ele;
+    sum = sum + eachNum; // also same as sum += eachNum
+  });
+
+  return sum;
+};
+
+
+console.log(arrowRestSum(3, 5, 6)); // => 14
+console.log(arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(arrowRestSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
