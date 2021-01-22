7e77d5b3433a21143f53bd667a8079289c4e4e0a
Jonathan <jonathannblackk@gmail.com>
2021-01-22 15:03:43 -0800

    04-averageValue

--- scope-problems-without-context-starter/problems/04-arrow-average-value.js
+++ scope-problems-without-context-starter/problems/04-arrow-average-value.js
@@ -10,8 +10,20 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// your code here!
+const arrowAvgValue = array => {
 
+  let sum = 0;
+  array.forEach(function (ele) {
+    let eachNum = ele;
+    sum = sum + eachNum
+  })
+  return sum / array.length;
+}
+
+
+console.log(arrowAvgValue([10, 20])); // => 15
+console.log(arrowAvgValue([2, 3, 7])); // => 4
+console.log(arrowAvgValue([100, 60, 64])); // => 74.66666666666667
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
